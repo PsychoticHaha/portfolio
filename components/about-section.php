3e0cf55554e64622386b00e233168797208667e6
Fanasina Ramalandimanana <fanasina@fulldigits.com>
2024-12-08 09:08:20 +0300

    WIP

---
+++ components/about-section.php
@@ -0,0 +1,24 @@
+<section class=about-me id=about-me-section>
+  <div class="section-wrapper">
+    <h2 class=whois>Who is RAF ?</h2>
+    <div class=text id=about-paragraph>
+      <p class=full-text>
+        RAF, a talented self-taught web developer who embarked on his coding journey in 2016. Despite taking a hiatus
+        between 2018 and 2022, his passion for programming and development has remained unwavering. Always on the
+        lookout for fresh challenges, RAF specializes in frontend development with a mastery of React JS.
+      </p>
+      <p class=full-text>
+        In the dynamic world of web development, RAF stands out as an individual who not only embraces challenges but
+        thrives on them. His journey showcases resilience and a continuous pursuit of excellence, evident in his
+        commitment to honing his skills.
+      </p>
+      <p>
+        P.S : My initials, RAF, stand for Ramalandimanana Antso Fanasina.
+      </p>
+      <div class=source></div>
+    </div>
+    <div class=btn-container>
+      <div class=more-less-btn id=read-more-about-me>Read less</div>
+    </div>
+  </div>
+</section>
---
+++ components/footer.php
@@ -0,0 +1,43 @@
+<footer>
+    <div class=copyright>
+        <div class=text>© &nbsp;<span class=year>
+                <?php echo date("Y"); ?>
+            </span> &nbsp; RAF</div>
+    </div>
+    <div class=container>
+        <div class=footer-section>
+            <img src=./images/raf_logo_white.png alt="RAF Logo" width=50>
+            <div class=quote>
+                "Great results comes along with a great work."
+            </div>
+        </div>
+        <div class="footer-section middle">
+            <div class=item>
+                <div class="icon location"></div>
+                <div class=text>Madagascar</div>
+            </div>
+            <a href=tel:+261344970553 class="item phone" target=_blank>
+                <div class="icon phone"></div>
+                <div class=text>+261344970553</div>
+            </a>
+        </div>
+        <div class=footer-section>
+            <a href=https://https://www.linkedin.com/in/fanasina-ramalandimanana-75110a236/ class=item target=_blank>
+                <div class="icon linkedin"></div>
+                <div class=text>Fanasina Ramalandimanana</div>
+            </a>
+            <a href=https://join.skype.com/invite/J4LXJ8KvFWf6 class=item target=_blank>
+                <div class="icon skype"></div>
+                <div class=text>Fanasina Ramalandimanana</div>
+            </a>
+            <a href=https://github.com/PsychoticHaha class="item github" target=_blank>
+                <div class="icon github"></div>
+                <div class=text>My Github</div>
+            </a>
+            <a href=mailto:hope.fanasina2@gmail.com class="item gmail" target=_blank>
+                <div class="icon gmail"></div>
+                <div class=text>hope.fanasina2@gmail.com</div>
+            </a>
+        </div>
+    </div>
+</footer>
